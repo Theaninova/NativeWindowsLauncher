62fb3b76f98e275b764e572587c10d5d394777d2
Wieland Schöbl <wulkanat@gmail.com>
2018-08-01 00:59:59 +0200

    Converting Kotlin Renderer to C++ (App only shows red now)

--- app/src/main/cpp/WindowsLauncher.h
+++ app/src/main/cpp/WindowsLauncher.h
@@ -3,6 +3,7 @@
 //
 
 #include <math.h>
+#include <vector>
 
 #ifndef NATIVEWINDOWSLAUNCHER_WINDOWSLAUNCHER_H
 #define NATIVEWINDOWSLAUNCHER_WINDOWSLAUNCHER_H
@@ -55,3 +56,71 @@ const float editModeZoom = -0.1f;
 const float editModeSelectedZoom = -0.05f;
 const float editModeAlpha = 0.8f;
 const float editModeSelectedAlpha = 1.0f;
+
+//TODO: Implement interpolator
+/*var fadeInInterpolator = AccExpInterpolator(fadeInNum1, fadeInNum2, fadeInNum3, fadeInLoc)
+var fadeOutInterpolator = AccExpInterpolator(fadeOutNum1, fadeOutNum2, fadeOutNum3, fadeOutLoc)
+var overscrollInterpolator = AccExpInterpolator(overscrollNum1, overscrollNum2, overscrollNum3, overscrollLoc)*/
+
+struct Tile {
+
+};
+
+std::vector<Tile> tiles;
+
+const int gridWidth = 6;
+const float leftSideMargin = 0.0104167f * 2.0f; //0.01041 6 Period  Measured
+const float rightSideMargin = 0.0118056f * 2.0f;//0.01180 5 Period  Measured
+const float tilesMargin = 0.0097222f * 2.0f;    //0.0097  2 Period  Measured
+const float topMargin = tilesMargin;
+float statusBarHeightPercentage = 0.0f;
+//float statusBarHeight = 0.0f;
+//TODO: setter or equivalent for statusBarHeight
+/*set (value) {
+        field = value
+        overscrollDist = 0.2090278f * 2 - topMargin - value
+}*/
+float navBarHeightPercentage = 0.0f;
+float navBarHeight = 0.0f;
+float overscrollDist = 0.2090278f * 2.0f - topMargin; //0,20902 / Period
+const double overscrollDuration = 0.5;
+float todoOverscrollDist = 0.0f;
+double overscrollElapsed = 0.0;
+
+float gridOverscrollHeight = 0.0f;
+//float gridHeight = 0.0f;
+//TODO: setter or equivalent for gridHeight
+/*set (value) {
+        field = value
+        gridOverscrollHeight = (value - (glGrid[3] - glGrid[2] - statusBarHeight - navBarHeight)) * -1
+}*/
+int gridHeightSpan = 0;
+
+float scrollDist = 0.0f;
+
+float tileSizeCache = 0.0f;
+float tileXPosChache[gridWidth];
+float tileAndMarginCache = 0.0f;
+
+float glGrid[4];
+
+int startTileIndex = 0;
+float rowsOnScreen = 0.0f;
+double animProgress = 0.0;
+double enterTemporalOffsets[relativeEnterRowZoomOffsetsSize];
+
+bool exiting = false;
+bool entering = false;
+
+float tapTolerance = 0.0005f;
+float tapPosition[2];
+bool tapInitiated = false;
+double timeSinceTap = 0.0;
+
+float exitStartPos = 0.0f;
+double exitAnimTime = 0.0;
+int exitTapTileLoc[2];
+
+bool appDrawer = false;
+bool editMode = false;
+int editModeSelectedTile = 0;
